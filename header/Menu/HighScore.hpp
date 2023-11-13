6916c3aa81ef3501913e0497b1fb3aab7f1a9704
nguyenquyking <nguyentrongquy0978172149@gmail.com>
2023-11-13 18:50:47 +0700

    Change a bit

--- header/Menu/HighScore.hpp
+++ header/Menu/HighScore.hpp
@@ -3,7 +3,7 @@
 
 #include <SFML/Graphics.hpp>
 
-class Highscore
+class HighScore
 {
 public:
 
--- source/Menu/Menu.cpp
+++ source/Menu/Menu.cpp
@@ -7,24 +7,23 @@ Menu::Menu()
 
 void Menu::draw(int state, sf::RenderWindow& mWindow)
 {
-    mWindow.clear();
     if(state==0)
     {
         mainscreen.draw(mWindow);
     }
-    else if(state=1)
+    else if(state==1)
     {
 
     }
-    else if(state=2)
+    else if(state==2)
     {
         
     }
-    else if(state=3)
+    else if(state==3)
     {
         
     }
-    else if(state=4)
+    else if(state==4)
     {
         
     }
